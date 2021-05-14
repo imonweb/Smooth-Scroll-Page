320bd0f2952851393411b2188f41f189ed1f7c41
imonweb <contact@imonweb.co.uk>
2021-05-14 21:38:21 +0100

    Smooth Scroll - JS

--- src/index.js
+++ src/index.js
@@ -12,3 +12,15 @@ import '../src/css/styles.css';
   }
 }) */
 import '../node_modules/smooth-scroll/dist/smooth-scroll.js';
+
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, far, fab) 
+
+dom.i2svg()
+
+// library.add(faCheck);
+dom.watch();
--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,19 @@ module.exports = {
         {
           test: /\.(png|svg|jpeg|jpg|gif|svg)$/i,
           type: "asset/resource"
-        }
+        },
+        {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
       ]
     },
      devtool: 'inline-source-map',
